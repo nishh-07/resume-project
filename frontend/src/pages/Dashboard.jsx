66f460abb51e204df0edcf237c19eb6fb7dd77cd
Nishan <nishanmc1184@gmail.com>
2026-05-03 14:16:53 +0530

    live backend url

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -24,7 +24,7 @@ function Dashboard() {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/upload",
+        "https://resume-project-gioa.onrender.com/upload",
         formData
       );
 
